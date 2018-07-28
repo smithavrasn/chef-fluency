1ab9b053d019cfa2340cc329fc3bfaaeab40d6ff
smitha <smitha@gmail.com>
2018-07-28 11:37:55 +0000

    Adding

---
+++ cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook:: postgressql
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+#
+# 1.Install package
+# 2.Execute when notified
+# Enable and start the service
+#
+package 'postgresql-server' do
+	 notifies :run, 'execute[postgresql-init]', :immediately
+end
+
+execute 'postgresql-init' do
+         command 'postgresql-setup initdb'
+	 action :nothing
+end
+
+service 'postgresql' do
+	 action [:enable, :start]
+end
